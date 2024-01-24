d15a35dbd872c601620c5f58756f99f821ca2d23
Sakib Malik <maliksakib347@gmail.com>
2024-01-24 07:32:32 +0530

    JAN/24

--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
-const app = require('./app')
-require('dotenv').config();
+const app = require("./app");
+require("dotenv").config();
 const port = process.env.PORT || 8080;
 
 app.listen(port, (err) => {
-    if (err) {
-        console.log("ERRROR: ", err)
-    }
-    console.log(`Listening on port ${port}`)
-})
+  if (err) {
+    console.log("ERRROR: ", err);
+  }
+  console.log(`Listening on port ${port}`);
+});
+// Export the express app
+module.exports = app;
